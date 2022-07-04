47e2d51e3e56f2647724053a1e61f21b62acaaea
UpgradeOfficial <odeyemiincrease@yahoo.com>
2022-07-04 05:09:09 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { response } = require("express");
+
+const app = express();
+
+app.use(express.json())
+
+const port = 8000;
+
+app.listen(port, () => {
+  console.log("We are live on " + port);
+});
+
+app.post("/split-payments/compute", (req, res) => {
+  
+  var request = req.body;      // your JSON
+  
+  var ans = request.SplitInfo;
+  var amount = request.Amount;
+  var percentage_array = [];
+  var ratio_array = [];
+  ratio_total = 0;
+  var ratio_amount;
+  var SplitBreakdown = [];
+
+  ans.forEach(function (item, index) {
+    if (item.SplitType === "PERCENTAGE") {
+      percentage_array.push(item);
+    } else if (item.SplitType === "RATIO") {
+      ratio_total += item.SplitValue;
+      ratio_array.push(item);
+    } else {
+      amount -= item.SplitValue;
+      SplitBreakdown.push({
+        SplitEntityId: item.SplitEntityId,
+        Amount: amount,
+      });
+    }
+  });
+
+  percentage_array.forEach(function (item, index) {
+    amount -= (item.SplitValue / 100) * amount;
+    SplitBreakdown.push({
+      SplitEntityId: item.SplitEntityId,
+      Amount: amount,
+    });
+  });
+
+  var ratio_temporary_amount = amount;
+
+  ratio_array.forEach(function (item, index) {
+    ratio_amount = (item.SplitValue / ratio_total) * ratio_temporary_amount;
+    amount -= ratio_amount;
+    SplitBreakdown.push({
+      SplitEntityId: item.SplitEntityId,
+      Amount: ratio_amount,
+    });
+  });
+
+  var response = {
+    ID: request.ID,
+    balance: amount,
+    SplitBreakdown: SplitBreakdown,
+  };
+  res.send(response)
+});
